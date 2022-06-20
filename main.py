1a667bcc910633468c648f569d5e0d7caaf3572d
User <sss>
2022-06-20 17:03:06 +0300

    test

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import requests
-import json
 
-headers = {'Authorization': 'N3 1a55e61b-dd7a-4acf-a94a-37d21d761af5'}
-data = {
-    'workflowId': '09872eef-6180-4f5f-9137-c33ce60ad416',
-    'name': 'Тест НСИ',
-    'initialTransitionId': '6ce8ec25-c09b-4bf1-81a9-541f749bac7c',
-    'processContext': '{}',
-    'roleContext': '{}'
-}
-response = requests.post("http://r78-test.zdrav.netrika.ru/tm-core/api/Commands/StartNewProcess",  headers = headers)
+headers = {'Authorization': 'N3 1a55e61b-dd7a-4acf-a94a-37d21d761af5', 'Content-Type': 'application/json', 'Accept': '*/*'}
+
+
+data = "{'workflowId': '09872eef-6180-4f5f-9137-c33ce60ad416','name': 'test','initialTransitionId': '6ce8ec25-c09b-4bf1-81a9-541f749bac7c','processContext': {}, 'RoleContext': {}}"
+url = 'http://r78-test.zdrav.netrika.ru/tm-core/api/Commands/StartNewProcess'
+response = requests.post(url, data=data.encode('UTF-8'), headers=headers)
 print(response.text)
 print(response.status_code)
-
-
