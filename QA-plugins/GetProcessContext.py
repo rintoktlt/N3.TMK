160778f25713982087b251be73c488ced50d0c2e
User <sss>
2022-06-20 17:47:43 +0300

    working

--- QA-plugins/GetProcessContext.py
+++ QA-plugins/GetProcessContext.py
@@ -3,8 +3,8 @@ import requests
 headers = {'Authorization': 'N3 1a55e61b-dd7a-4acf-a94a-37d21d761af5', 'Content-Type': 'application/json', 'Accept': '*/*'}
 
 
-data = "{'RoleContext': {},'ProcessId': '4978798e-296b-4c20-a5ba-4c15f09b5d13'}"
-url ="http://r78-test.zdrav.netrika.ru/tm-core/api/Queries/GetProcessContext"
+data = "{'RoleContext': {},'ProcessId': 'fec15446-4e43-45f7-82e7-9bc843b24521'}"
+url = "http://r78-test.zdrav.netrika.ru/tm-core/api/Queries/GetProcessContext"
 response = requests.post(url, data=data, headers=headers)
 print(response.text)
 print(response.status_code)
--- main.py
+++ main.py
@@ -6,5 +6,7 @@ headers = {'Authorization': 'N3 1a55e61b-dd7a-4acf-a94a-37d21d761af5', 'Content-
 data = "{'workflowId': '09872eef-6180-4f5f-9137-c33ce60ad416','name': 'test','initialTransitionId': '6ce8ec25-c09b-4bf1-81a9-541f749bac7c','processContext': {}, 'RoleContext': {}}"
 url = 'http://r78-test.zdrav.netrika.ru/tm-core/api/Commands/StartNewProcess'
 response = requests.post(url, data=data.encode('UTF-8'), headers=headers)
-print(response.text)
+response_dict = response.json()
+print(response_dict)
+print(response_dict["errorCode"])
 print(response.status_code)
